609444881acbf4c0c700f9af27625f40e92e36bf
Tarun93 <tarun.kapadia@micromaxinfo.com>
2018-03-31 14:23:55 +0530

    FFS get in there
    Signed-off-by: Tarun93 <tarun.kapadia@micromaxinfo.com>

--- lineage.mk
+++ lineage.mk
@@ -17,17 +17,21 @@
 
 $(call inherit-product, device/yu/garlic/full_garlic.mk)
 
-# Inherit some common Cosmic-OS stuff.
-$(call inherit-product, vendor/cos/common.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := cos_garlic
+PRODUCT_NAME := lineage_garlic
 BOARD_VENDOR := yu
 
 PRODUCT_GMS_CLIENTID_BASE := android-micromax
 
+TARGET_VENDOR_PRODUCT_NAME := garlic
+
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=YU/YU5040/YU5040:7.1.1/NMF26F/1502798695:user/release-keys \
     PRIVATE_BUILD_DESC="p7201-user 7.1.1 NMF26F 1503040709 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and boot image
+BUILD_FINGERPRINT := "YU/YU5040/YU5040:7.1.1/NMF26F/1502798695:user/release-keys"
 endif
