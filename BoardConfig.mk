3dae4ed3112b3b4c1445058c50f69f561ba632b6
vjspranav <pranavasri@live.in>
2018-05-12 11:05:59 +0530

    Changes for rw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,10 +57,9 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 -
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CONFIG := lineage_garlic_defconfig
 TARGET_KERNEL_SOURCE := kernel/yu/msm8937
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Clang
-INTERNAL_LOCAL_CLANG_EXCEPTION_PROJECTS := external/busybox/
+#INTERNAL_LOCAL_CLANG_EXCEPTION_PROJECTS := external/busybox/
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
